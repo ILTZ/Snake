214217cf27e957eb782786a698446b574ba2d1c9
ILTZ <63543530+ILTZ@users.noreply.github.com>
2022-10-25 21:04:22 +0500

    --add some custom lvls

--- Snake/ConfigLoader.cpp
+++ Snake/ConfigLoader.cpp
@@ -1,18 +1,18 @@
-#ifndef NDEBUG
-
-#include <iostream>
-
-#define JSON_TRY_USER if (true)
-#define JSON_CATCH_USER(exception) if(false)
-#define JSON_THROW_USER(exception)                           \
-    {std::clog << "Error in " << __FILE__ << ":" << __LINE__ \
-               << " (function " << __FUNCTION__ << ") - "    \
-               << (exception).what() << std::endl;			 \
-				throw exception;}
-
-#endif // !NDEBUG
+//#ifndef NDEBUG
+//
+//#include <iostream>
+//
+//#define JSON_TRY_USER if (true)
+//#define JSON_CATCH_USER(exception) if(false)
+//#define JSON_THROW_USER(exception)                           \
+//    {std::clog << "Error in " << __FILE__ << ":" << __LINE__ \
+//               << " (function " << __FUNCTION__ << ") - "    \
+//               << (exception).what() << std::endl;			 \
+//				throw exception;}
+//
+//#endif // !NDEBUG
 																
-#include "ConfigLoader.h"																		\
+#include "ConfigLoader.h"																		
 																	
 using json = nlohmann::json;
 
